d3214fcfbdd8097325bfc70f41214b0410b0292d
lewismurphy <hjmurphy24@gmail.com>
2023-06-11 13:12:37 -0500

    Added color changing settings to the game as well as displaying the settings on the title screen.

--- main.py
+++ main.py
@@ -4,39 +4,86 @@ import random
 
 def score():  # Function to display scores
 
-    player_score_surface = game_font.render(f'{player_score}', False, 'White')
+    player_score_surface = game_font.render(f'{player_score}', False, color)
     player_score_rect = player_score_surface.get_rect(center=(375, 40))
     screen.blit(player_score_surface, player_score_rect)
 
-    computer_score_surface = game_font.render(f'{computer_score}', False, 'White')
+    computer_score_surface = game_font.render(f'{computer_score}', False, color)
     computer_score_rect = computer_score_surface.get_rect(center=(625, 40))
     screen.blit(computer_score_surface, computer_score_rect)
 
 
 def final_score():  # Function to display final scores
 
-    player_final_score_surface = game_font.render(f' User Score: {player_score}', False, 'White')
+    player_final_score_surface = game_font.render(f' User Score: {player_score}', False, color)
     player_final_score_rect = player_final_score_surface.get_rect(center=(500, 100))
     screen.blit(player_final_score_surface, player_final_score_rect)
 
-    computer_final_score_surface = game_font.render(f' Computer Score: {computer_score}', False, 'White')
+    computer_final_score_surface = game_font.render(f' Computer Score: {computer_score}', False, color)
     computer_final_score_rect = computer_final_score_surface.get_rect(center=(500, 300))
     screen.blit(computer_final_score_surface, computer_final_score_rect)
 
-    replay_message = game_font.render('Press Space to play again!', False, 'White')
+    replay_message = game_font.render('Press Space to play again!', False, color)
     replay_message_rect = replay_message.get_rect(center=(500, 500))
     screen.blit(replay_message, replay_message_rect)
 
 
 def title_screen_text():  # Function to display the title screen info
 
-    title_surface = title_screen_font.render('PONG', False, 'White')
+    title_surface = title_screen_font.render('PONG', False, color)
     title_surface_rect = title_surface.get_rect(center=(500, 100))
     screen.blit(title_surface, title_surface_rect)
 
-    play_message_surface = game_font.render('Press Space to Play', False, 'White')
+    play_message_surface = game_font.render('Press Space to Play', False, color)
     play_message_surface_rect = play_message_surface.get_rect(center=(500, 500))
     screen.blit(play_message_surface, play_message_surface_rect)
+
+
+def color_change():  # Function to display color changing information
+
+    color_change_title_surface = color_change_font.render('Color Settings', False, color)
+    color_change_title_rect = color_change_title_surface.get_rect(center=(500, 200))
+    screen.blit(color_change_title_surface, color_change_title_rect)
+
+    color_change_red = color_change_font.render('1 - Red', False, (255, 0, 0))
+    color_change_red_rect = color_change_red.get_rect(center=(450, 225))
+    screen.blit(color_change_red, color_change_red_rect)
+
+    color_change_green = color_change_font.render('2 - Green', False, (0, 255, 0))
+    color_change_green_rect = color_change_green.get_rect(center=(450, 250))
+    screen.blit(color_change_green, color_change_green_rect)
+
+    color_change_blue = color_change_font.render('3 - Blue', False, (0, 0, 255))
+    color_change_blue_rect = color_change_blue.get_rect(center=(450, 275))
+    screen.blit(color_change_blue, color_change_blue_rect)
+
+    color_change_yellow = color_change_font.render('4 - Yellow', False, (255, 255, 0))
+    color_change_yellow_rect = color_change_yellow.get_rect(center=(450, 300))
+    screen.blit(color_change_yellow, color_change_yellow_rect)
+
+    color_change_orange = color_change_font.render('5 - Orange', False, (255, 165, 0))
+    color_change_orange_rect = color_change_orange.get_rect(center=(450, 325))
+    screen.blit(color_change_orange, color_change_orange_rect)
+
+    color_change_purple = color_change_font.render('6 - Purple', False, (160, 32, 240))
+    color_change_purple_rect = color_change_purple.get_rect(center=(550, 225))
+    screen.blit(color_change_purple, color_change_purple_rect)
+
+    color_change_pink = color_change_font.render('7 - Pink', False, (255, 192, 203))
+    color_change_pink_rect = color_change_pink.get_rect(center=(550, 250))
+    screen.blit(color_change_pink, color_change_pink_rect)
+
+    color_change_cyan = color_change_font.render('8 - Cyan', False, (0, 100, 100))
+    color_change_cyan_rect = color_change_cyan.get_rect(center=(550, 275))
+    screen.blit(color_change_cyan, color_change_cyan_rect)
+
+    color_change_brown = color_change_font.render('9 - Brown', False, (150, 75, 0))
+    color_change_brown_rect = color_change_brown.get_rect(center=(550, 300))
+    screen.blit(color_change_brown, color_change_brown_rect)
+
+    color_change_white = color_change_font.render('0 - White', False, (255, 255, 255))
+    color_change_white_rect = color_change_white.get_rect(center=(550, 325))
+    screen.blit(color_change_white, color_change_white_rect)
 
 
 pygame.init()  # Starts Pygame
@@ -84,6 +131,7 @@ collision_tolerance = 10
 
 game_font = pygame.font.Font('Minecraft.ttf', 50)  # Loads in the font for the scores
 title_screen_font = pygame.font.Font('Minecraft.ttf', 90)  # Loads the font for the title screen
+color_change_font = pygame.font.Font('Minecraft.ttf', 20)  # Loads the font for the color changing text
 
 ball_hit_paddle = pygame.mixer.Sound('sounds_ping_pong_8bit/ping_pong_8bit_beeep.ogg')
 score_sound = pygame.mixer.Sound('sounds_ping_pong_8bit/ping_pong_8bit_peeeeeep.ogg')
@@ -102,6 +150,50 @@ while True:
             pygame.quit()
 
             exit()
+
+        if event.type == pygame.KEYDOWN:
+
+            # Changing the color of the game
+
+            if event.key == pygame.K_0:
+
+                color = (255, 255, 255)  # White
+
+            if event.key == pygame.K_1:
+
+                color = (255, 0, 0)  # Red
+
+            if event.key == pygame.K_2:
+
+                color = (0, 255, 0)  # Green
+
+            if event.key == pygame.K_3:
+
+                color = (0, 0, 255)  # Blue
+
+            if event.key == pygame.K_4:
+
+                color = (255, 255, 0)  # Yellow
+
+            if event.key == pygame.K_5:
+
+                color = (255, 165, 0)  # Orange
+
+            if event.key == pygame.K_6:
+
+                color = (160, 32, 240)  # Purple
+
+            if event.key == pygame.K_7:
+
+                color = (255, 192, 203)  # Pink
+
+            if event.key == pygame.K_8:
+
+                color = (0, 100, 100)  # Cyan
+
+            if event.key == pygame.K_9:
+
+                color = (150, 75, 0)  # Brown
 
         if game_active:
 
@@ -348,6 +440,8 @@ while True:
             screen.fill((0, 0, 0))
 
             title_screen_text()
+
+            color_change()
 
         else:
 
